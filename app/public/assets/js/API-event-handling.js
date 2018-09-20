45da0b3ce66f5132b786714df5ae53e5d4284949
byt3_h34d <mr.tybopp@gmail.com>
2018-09-19 22:11:53 -0500

    Event handling for city search in place, just need new search bar.

--- app/public/assets/js/API-event-handling.js
+++ app/public/assets/js/API-event-handling.js
@@ -3,34 +3,35 @@
 // URL parameter. Our server then performs the search to grab that character from the Database.
 
 // when user hits the city-search-btn
-$("#search-btn").on("click", function() {
-    // save the character they typed into the character-search input
-    let searchedCity = $("#city-search")
-      .val()
-      .trim();
-        
-    // run an AJAX GET-request for our servers api,
-    // including the user's character in the url
-    $.get("/api/" + searchedCharacter, function(data) {
-      // log the data to our console
-      console.log(data);
-      // empty to well-section before adding new content
-      $("#well-section").empty();
-      // if the data is not there, then return an error message
-      if (!data) {
-        $("#well-section").append("<h2> The force is not strong with this one. Your character was not found. </h2>");
-      }
-      else {
-        // otherwise
-        // append the character name
-        $("#well-section").append("<h2>" + data.name + "</h2>");
-        // the role
-        $("#well-section").append("<h3>Role: " + data.role + "</h3>");
-        // the age
-        $("#well-section").append("<h3>Age: " + data.age + "</h3>");
-        // and the force points
-        $("#well-section").append("<h3>Force Points: " + data.forcePoints + "</h3>");
-      }
-    });
+$("#search-btn").on("click", function () {
+  event.preventDefault();
+  // save the character they typed into the character-search input
+  let searchedCity = $("#city-search")
+    .val()
+    .trim();
+
+  // Using a RegEx Pattern to remove spaces from searchedCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  searchedCity = searchedCity.replace(/\s+/g, "").toLowerCase();
+
+  // run an AJAX GET-request for our servers api,
+  // including the user's character in the url
+  $.get("/api/" + searchedCity, function (data) {
+    // log the data to our console
+    console.log(data);
+    // empty to well-section before adding new content
+    $("#test-data-dump").empty();
+    // if the data is not there, then return an error message
+    if (!data) {
+      $("#test-data-dump").append("<h2> No data found. </h2>");
+    } else {
+      // otherwise
+      // append the character name
+      $("#test-data-dump").append("<h2>" + data.Areaname + "</h2>");
+      // the role
+      $("#test-data-dump").append("<h3>Data Set #1: " + data.STCOU + "</h3>");
+      // the age
+      $("#test-data-dump").append("<h3>Data Set #2: " + data.PST100209D + "</h3>");
+    }
   });
-  
+});
