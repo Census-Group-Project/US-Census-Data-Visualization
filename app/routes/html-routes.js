36f5e5fac2dd40e7aaeacf42b30f7ef26d9c0191
byt3_h34d <mr.tybopp@gmail.com>
2018-09-19 19:55:11 -0500

    site works. Made comment changes in the html-routes file.

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,13 +1,11 @@
 // Dependencies
 const path = require("path");
 
-
-// Routes
+ // The below route handles the index HTML page that the user initially gets sent to.
 module.exports = function(app) {
 
-  // The below route handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
+  // index route loads index.html
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/assets/views/index.html"));
   });
