ef8d8af961d4d910f1ffe8a1707299efcb120d9d
BogieBogard <mbogard@utexas.edu>
2018-09-21 18:04:00 -0500

    Trying to fix heroku deployment by updating connection.js

--- app/config/connection.js
+++ app/config/connection.js
@@ -18,3 +18,24 @@ if (config.use_env_variable) {
     config
   );
 }
+
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
+  })
+  .forEach(function(file) {
+    var model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
