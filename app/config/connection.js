a1efe8c05dde52d96cd9e2eeaf9fdcddb8fbc6aa
BogieBogard <mbogard@utexas.edu>
2018-09-21 17:55:48 -0500

    Updated connection.js

---
+++ app/config/connection.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.json")[env];
+var db = {};
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config_password,
+    config
+  );
+}
