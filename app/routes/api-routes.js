52229220bf7203b1759fae20069b3cbd5594b921
byt3_h34d <mr.tybopp@gmail.com>
2018-09-22 22:58:33 -0500

    Fixing issue with DB search. Reduced_Name column has been added and is new being used as the search target. Areaname is still being used to display properly formated city and state.

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -7,20 +7,18 @@ module.exports = function (app) {
   app.get("/api/:city?", function (req, res) {
     if (req.params.city) {
 
-      // Re-construct incoming search params for database:
-      // 1. Store the search params into a variable.
+      // Re-construct search params for database:
+      // 1. Store the incoming search parameters into a variable.
       let searchParams = req.params.city;
-
       // 2. Split the search variable into an array of two separate strings (city and the state) at the wildcard character '&'.
       searchParams = searchParams.split("&");
-
       // 3. Re-define the search variable as the two strings connected with a ", " in between city and state strings in order to pass the exact format required for the database search.
-      searchParams = searchParams[0] + ", " + searchParams[1];
+      searchParams = searchParams[0] + "," + searchParams[1];
 
       // Pass the search params to sequelize to perform the search.
       Census.findOne({
         where: {
-          Areaname: searchParams
+          Reduced_Name: searchParams
         }
       }).then(function (results) {
         return res.json(results);
@@ -28,7 +26,7 @@ module.exports = function (app) {
     } else {
       Census.findOne({
         where: {
-          Areaname: 'UNITED STATES'
+          areaname: 'UNITED STATES'
         }
       }).then(function (results) {
         return res.json(results);
