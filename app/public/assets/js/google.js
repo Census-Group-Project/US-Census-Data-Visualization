52eb938d3a6413fc1bc74f20107792320910f12a
Amanda <tranamanda14@gmail.com>
2018-09-25 21:45:29 -0500

    fixed api times, cleaned up main index

--- app/public/assets/js/google.js
+++ app/public/assets/js/google.js
@@ -2,16 +2,16 @@
 var timezoneNY;
 var timezoneLA;
 var timezoneChicago;
-var timezoneHou;
-
-
-function square(number) {
-    return number * number;
-}
+var timezoneSea;
+var timezoneSanta;
+var timezoneBost;
 
 
 
-function setTimezoneTiles(loc, locNY, locChi, locH) {
+
+
+
+function setTimezoneTiles(loc, locNY, locChi, locH, locSan, locBost) {
 
     var targetDate = new Date(); // Current date/time of user computer
     var timestamp = targetDate.getTime() / 1000 + targetDate.getTimezoneOffset() * 60; // Current UTC date/time expressed as seconds since midnight, January 1, 1970 UTC
@@ -20,13 +20,16 @@ function setTimezoneTiles(loc, locNY, locChi, locH) {
     var apicall = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + loc + '&timestamp=' + timestamp + '&key=' + apikey
     var apicall1 = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + locNY + '&timestamp=' + timestamp + '&key=' + apikey
     var apicall2 = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + locChi + '&timestamp=' + timestamp + '&key=' + apikey
-    var apicall3 = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + locChi + '&timestamp=' + timestamp + '&key=' + apikey
-  
+    var apicall3 = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + locH + '&timestamp=' + timestamp + '&key=' + apikey
+    var apicall4 = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + locSan + '&timestamp=' + timestamp + '&key=' + apikey
+    var apicall5 = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + locBost + '&timestamp=' + timestamp + '&key=' + apikey
   
     callApiAndUpdateText(apicall, "timezoneLA", timestamp);
     callApiAndUpdateText(apicall1, "timezoneNY", timestamp);
     callApiAndUpdateText(apicall2, "timezoneChicago", timestamp);
-    callApiAndUpdateText(apicall3, "timezoneChicago", timestamp);
+    callApiAndUpdateText(apicall3, "timezoneSea", timestamp);
+    callApiAndUpdateText(apicall4, "timezoneSanta", timestamp);
+     callApiAndUpdateText(apicall5, "timezoneBost", timestamp);
  
     
 }
@@ -55,12 +58,13 @@ function callApiAndUpdateText(apicall, timezoneName, timestamp) {
 $("#search-btn").on("click", function () {
     // get some coordinates
     var loc = '34.0522, -118.2437';
-    var locNY = '40.7128, 74.0060';
-    var locChi = '41.8781, 87.6298'; 
-    var locH = '29.7604, 95.3698';
-    
+    var locNY = '40.7128, -74.0060';
+    var locChi = '41.8781, -87.6298'; 
+    var locH = '32.7767, -96.7970';
+    var locSan = '34.0195, -118.4912';
+    var locBost= '42.3601, -71.0589';
     // call our function, which is going to MAKE THE API REQUEST, and UPDATE THE TEXT
-    setTimezoneTiles(loc, locNY, locChi,); 
+    setTimezoneTiles(loc, locNY, locChi, locH, locSan, locBost); 
 
 });
 
